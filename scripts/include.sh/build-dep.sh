f5a1e8eb0904ec4842e4fd9ae432df9beddcaa26
zhouzhongguang <zhouzhongguang@hikvision.com.cn>
2023-02-21 18:32:22 +0800

    最低支持iOS 12

--- scripts/include.sh/build-dep.sh
+++ scripts/include.sh/build-dep.sh
@@ -8,7 +8,7 @@ build_git_ios()
 
 #  simarchs="i386 x86_64"
   simarchs="x86_64"
-  sdkminversion="9.0"
+  sdkminversion="12.0"
   sdkversion="`xcodebuild -showsdks 2>/dev/null | grep iphoneos | sed 's/.*iphoneos\(.*\)/\1/'`"
   devicearchs="arm64"
 
